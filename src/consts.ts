16bf0ee5e68d8ec6dea445a288993f3070714cd6
bottlenix <bottlenix@gmail.com>
2025-01-29 08:16:40 -0500

    add global constants for site title, description, navigation, and social links

---
+++ src/consts.ts
@@ -0,0 +1,15 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NAV_LINKS = [
+  { url: "/", title: "Nav-Home" },
+  { url: "/about/", title: "Nav-About" },
+  { url: "/blog/", title: "Nav-Blog" },
+];
+export const SOCIAL_LINKS = [
+    { platform: "twitter", username: "astrodotbuild" },
+    { platform: "github", username: "withastro" },
+    { platform: "youtube", username: "astrodotbuild" },
+];
